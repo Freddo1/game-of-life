a3a229e96ee1657a460c8665fc0e51ad7686444e
fredrik.julin <fredrik.julin@paradoxinteractive.com>
2021-08-13 15:41:23 +0200

    Prepare getting mouse position for creating universe

--- www/index.js
+++ www/index.js
@@ -1,9 +1,18 @@
 import {Universe} from "wasm-game-of-life";
 
 const pre = document.getElementById("game-of-life-canvas");
-const universe = Universe.new(264,264);
+const universe = Universe.new(264, 264); // Make the universe but don't initiate th cells just yet.
+pre.addEventListener('click', (e) => {
+   getRelativeCoords(e);
+   // Init small local cluster of cells.
+});
 
 console.log(universe);
+
+const getRelativeCoords = (event) => {
+    console.log(event.offsetX, event.offsetY);
+    return {x: event.offsetX, y: event.offsetY};
+}
 
 const renderLoop = () => {
     pre.textContent = universe.render();
